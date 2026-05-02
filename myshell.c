33f47776bb86c0b3a6aa6e6225d1f0a56c227ebd
ohadargavan <ohad.argavan@gmail.com>
2026-05-02 13:17:04 +0300

    execute function

--- myshell.c
+++ myshell.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <linux/limits.h> // for PATH_MAX
+#include <string.h>
+#include "LineParser.h"
+#include <stdlib.h> //for exiting with error
+
+void execute(cmdLine *pCmdLine) {
+    // run the command via 'execv'
+    //arguments[0] is the name of the command (and '->arguments' is the arguments array)
+    if (execv(pCmdLine->arguments[0], pCmdLine->arguments) == -1) { 
+        perror("Error executing command"); // print error in case of failure
+        exit(1); //exit with error
+    }
+}
 
 int main(int argc, char **argv) {
     char cwd[PATH_MAX];
+    char input [2048];
 
     while(1) {
         // gets the current path
@@ -14,8 +27,24 @@ int main(int argc, char **argv) {
             perror("getcwd error");
         }
         
-        // Here will be the code for next parts
+          // read input from user
+        if (fgets(input, 2048, stdin) == NULL) {
+            break; // in case of error or end of file
+        }
+
+        // check if user want to quit
+         //we use strncmp to check only first 4 chars (to not include new line for example)
+        if (strncmp(input, "quit", 4) == 0) {
+            break; // break the loop and exit the program
+        }
+        //Turn the input into a cmdLine structure
+        cmdLine *parsedLine = parseCmdLines(input);
+
+        execute(parsedLine);
+        freeCmdLines(parsedLine); //Free allocated memory
+
     }
     
     return 0;
 }
+
